50ba067047fb63f8b9776ebb3c0b508cd6af6946
Arsène Lapostolet <arsene@lapostolet.fr>
2020-06-08 16:59:09 +0200

    starting to play with replay parser

---
+++ ModernPlayerManagementAPI/Controllers/ReplaysController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using ModernPlayerManagementAPI.Models.DTOs;
+using RocketLeagueReplayParser;
+
+namespace ModernPlayerManagementAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReplaysController:ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Upload(IFormFile file)
+        {
+            // var stream = new MemoryStream();
+            // file.CopyTo(stream);
+            
+            var replay = Replay.Deserialize(file.OpenReadStream());
+            var playerStats = (List<PropertyDictionary>) replay.Properties.GetValueOrDefault("PlayerStats").Value;
+            var names = playerStats.Select(v => v.GetValueOrDefault("Name").Value.ToString());
+
+            return Ok(replay.Properties);
+        }
+    }
+}

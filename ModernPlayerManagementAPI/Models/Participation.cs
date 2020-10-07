409288dff2688064e27a371a227d193f4d98554e
Arsène Lapostolet <arsene@lapostolet.fr>
2020-10-07 19:29:51 +0200

    participations is now master detail

--- ModernPlayerManagementAPI/Models/Participation.cs
+++ ModernPlayerManagementAPI/Models/Participation.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ModernPlayerManagementAPI.Models
@@ -6,6 +7,7 @@ namespace ModernPlayerManagementAPI.Models
     public class Participation : BaseEntity
     {
         public bool Confirmed { get; set; }
+        [Required]
         public Guid EventId { get; set; }
         public Guid UserId { get; set; }
         [ForeignKey("UserId")] public User User { get; set; }
